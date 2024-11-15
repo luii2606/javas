8dd92e346d82a28cefa3f5fcf5be30fb969fc9d7
luii2606 <valemidx@gmail.com>
2024-11-15 11:31:20 -0500

    falta booleanos y más

---
+++ ejercicio.js
@@ -0,0 +1,43 @@
+// var
+
+
+var gato = "Draco"; // primera declaracion
+gato = "Satanás"; // redeclaro la variable gato
+console.log (gato); 
+
+// let
+let name = "Luisa";
+
+	if(true) {
+		let name = "Jonh";
+		console.log(name); // Jonh
+	}
+
+
+console.log(name) // Luisa
+
+//const
+const deporte = "Fútbol";
+console.log( "deporte:" + deporte);
+
+
+console.log('Ella dijo: “Adios!”')
+
+// strings
+
+texto = "Hola, me llamo Luisa y tengo 15 años";
+texto = "$#2*";
+console.log(texto); 
+
+//Template Strings
+
+cadena= 'Habia una vez una iguana, \
+con una ruana de lana \
+peinandose la melena';
+console.log(cadena); 
+
+// Numbers
+num=234;
+num=-100;
+num= 60;
+console.log(num);
